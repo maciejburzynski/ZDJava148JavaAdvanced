8d3c860f94eb9c040ed27b33782ec48c0852874d
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-07-30 11:09:10 +0200

    Add enum and access modifiers

---
+++ Coding/Java/composition/Color.java
@@ -0,0 +1,20 @@
+package composition;
+
+public enum Color {
+    RED("Red"),
+    BLUE("Blue"),
+    WHITE("White"),
+    GREEN("Green"),
+    BLACK("Black"),
+    ORANGE("Orange");
+
+    private String stringValue;
+
+    Color(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+}
